41419d5307175ee0a1d18cd32acd37ebef8d3d5e
Milcah <milcah@macbook-air.lan>
2020-11-19 20:16:41 -0800

    finish server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+
+const routes = require('./routes')
+
+const port = process.env.PORT || 4000
+const app = express()
+
+const corsOptions = {
+  origin: `http://localhost:3000`
+}
+
+app.use(express.json())
+app.use(cors(corsOptions))
+
+app.use('api/v1/strains', routes.strains)
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))
