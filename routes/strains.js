680bdf1048b79c7ac2c174edb72d10d4f433d407
Milcah <milcah@macbook-air.lan>
2020-11-19 20:20:33 -0800

    add strains routes and routes entry point

---
+++ routes/strains.js
@@ -0,0 +1,10 @@
+const router = require("express").Router()
+const ctrl = require('../controllers')
+
+router.get('/', ctrl.strains.index)
+router.get('/:id', ctrl.strains.show)
+router.post('/', ctrl.strains.create)
+router.put('/:id', ctrl.strains.update)
+router.delete('/:id', ctrl.strains.destroy)
+
+module.exports = router
