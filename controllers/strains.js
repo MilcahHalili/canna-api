8ab52328bf7a8036c84f05bff8dbbe215bec9e0e
Milcah <milcah@macbook-air.lan>
2020-12-11 21:29:05 -0800

    add show and create functions

--- controllers/strains.js
+++ controllers/strains.js
@@ -11,6 +11,30 @@ const index = (req, res) => {
     })
 }
 
+const show = (req, res) => {
+  db.Strain.findById(req.params.id)
+    .then(foundStrain => {
+      res.json({ strain: foundStrain })
+    })
+    .catch(err => {
+      console.log('Error in strains.show: ', err)
+      res.json({ Error: 'Unable to get strain data' })
+    })
+}
+
+const create = (req, res) => {
+  db.Strain.create(req.body)
+    .then(savedStrain => {
+      res.status(201).json({ strain: savedStrain })
+    })
+    .catch(err => {
+      console.log('Error in strain.create: ', err)
+      res.json({ Error: 'Unable to create data' })
+    })
+}
+
 module.exports = {
-  index
+  index,
+  show,
+  create
 }
--- routes/strains.js
+++ routes/strains.js
@@ -2,8 +2,8 @@ const router = require('express').Router()
 const ctrl = require('../controllers')
 
 router.get('/', ctrl.strains.index)
-// router.get('/:id', ctrl.strains.show)
-// router.post('/', ctrl.strains.create)
+router.get('/:id', ctrl.strains.show)
+router.post('/', ctrl.strains.create)
 // router.put('/:id', ctrl.strains.update)
 // router.delete('/:id', ctrl.strains.destroy)
 
